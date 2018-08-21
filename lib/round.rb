aa0c0acce7d3a46ac4dfae742fcbefc5b636b7e4
Mary Bork <maryebork@outlook.com>
2018-08-20 18:01:03 -0600

    Round Class File/Edit Format

--- lib/round.rb
+++ lib/round.rb
@@ -11,7 +11,6 @@ class Round
     @deck = deck
     @guesses = []
     @correct_guesses = []
-
   end
 
   def current_card
@@ -23,26 +22,23 @@ class Round
     response = "#{response[:value]} of #{response[:suit]}"
     current_guess = Guess.new(response, current_card)
 
-    if current_guess.correct? == true
-      @correct_guesses << current_guess
-      @guesses << current_guess
-    else
-      @guesses << current_guess
-    end
+      if current_guess.correct? == true
+        @correct_guesses << current_guess
+        @guesses << current_guess
+      else
+        @guesses << current_guess
+      end
 
     return current_guess
   end
 
   def number_correct
-    @correct_guesses.count
+     @correct_guesses.count
   end
 
   def percent_correct
      number = number_correct.to_f/ @guesses.count.to_f
      number * 100
   end
-
-
-
 
 end
